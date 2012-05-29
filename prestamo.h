14ec377c298add0e3db4ae8e8f9ec180c116dd5a
ajclopez9 <ajclopez9@gmail.com>
2012-05-29 23:37:43 +0000

    Prestamo integrado, falta realizar el prestamo en bases de datos!

---
+++ prestamo.h
@@ -0,0 +1,71 @@
+#ifndef PRESTAMO_H
+#define PRESTAMO_H
+
+#include <QWidget>
+#include <QLabel>
+#include <QLineEdit>
+#include <QComboBox>
+#include <QCalendarWidget>
+#include <QPushButton>
+#include <QMessageBox>
+
+class Prestamo : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit Prestamo(QWidget *parent = 0);
+    void initGUI(QWidget *);
+    void realizarPrestamo();
+    void visible(bool );
+
+private slots:
+    void slotValidate();
+    void slotCalendar();
+    void slotDate(QDate );
+    void slotRegistrar();
+
+private:
+    QLabel *lblCota;
+    QLineEdit *lineEditCota;
+
+    QLabel *lblAutor;
+    QLineEdit *lineEditAutor;
+
+    QLabel *lblTitulo;
+    QLineEdit *lineEditTitulo;
+
+    QLabel *lblTipoPrestamo;
+    QComboBox *comboBoxTipoPrestamo;
+
+    QLabel *lblGrado;
+    QComboBox *comboBoxGrado;
+
+    QLabel *lblCedula;
+    QLineEdit *lineEditCedula;
+
+    QLabel *lblFechaP;
+    QLineEdit *lineEditFechaP;
+
+    QLabel *lblFechaE;
+    QLineEdit *lineEditFechaE;
+
+    QLabel *lblCategoria;
+    QLineEdit *lineEditCategoria;
+
+    QLabel *lblCantidad;
+    QLineEdit *lineEditCantidad;
+
+    QLabel *lblStatus;
+    QLineEdit *lineEditStatus;
+
+    QLabel *lblResponsable;
+    QComboBox *comboBoxResponsable;
+
+    QCalendarWidget *calendarWidget;
+
+    QPushButton *btnCalendar;
+    QPushButton *btnRegistrar;
+
+};
+
+#endif // PRESTAMO_H
